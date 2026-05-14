a201ab7973241909acfac40448bda355efa9a22a
fiore <fiore@192.168.40.10>
2026-05-13 21:42:59 -0600

    Ejercicio en clase1

---
+++ src/u1/Genero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package u1;
+
+/**
+ *
+ * @author fiore
+ */
+public enum Genero {
+    FICCION, 
+    CIENCIA,
+    HISTORIA,
+    INFANTIL,
+    COMEDIA
+}
